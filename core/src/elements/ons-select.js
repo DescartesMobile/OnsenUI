434be97ea6e359c6cf19db5fa71b48bd6e1a28b9
misterjunio <ruca.33.maia@gmail.com>
2017-01-17 10:56:22 +0900

    added disabled attribute

--- core/src/elements/ons-select.js
+++ core/src/elements/ons-select.js
@@ -31,72 +31,45 @@ const defaultClassName = 'select';
 /**
  * @element ons-select
  * @category form
- * @modifier outline
- *   [en]Select with outline and transparent background[/en]
- *   [ja]アウトラインを持ったボタンを表示します。[/ja]
- * @modifier light
- *   [en]Button that doesn't stand out.[/en]
- *   [ja]目立たないボタンを表示します。[/ja]
- * @modifier quiet
- *   [en]Button with no outline and or background..[/en]
- *   [ja]枠線や背景が無い文字だけのボタンを表示します。[/ja]
- * @modifier cta
- *   [en]Button that really stands out.[/en]
- *   [ja]目立つボタンを表示します。[/ja]
- * @modifier large
- *   [en]Large button that covers the width of the screen.[/en]
- *   [ja]横いっぱいに広がる大きなボタンを表示します。[/ja]
- * @modifier large--quiet
- *   [en]Large quiet button.[/en]
- *   [ja]横いっぱいに広がるquietボタンを表示します。[/ja]
- * @modifier large--cta
- *   [en]Large call to action button.[/en]
- *   [ja]横いっぱいに広がるctaボタンを表示します。[/ja]
  * @modifier material
- *   [en]Material Design button[/en]
- *   [ja]マテリアルデザインのボタン[/ja]
- * @modifier material--flat
- *   [en]Material Design flat button[/en]
- *   [ja]マテリアルデザインのフラットボタン[/ja]
+ *  [en]Displays a Material Design select input.[/en]
+ *  [ja][/ja]
+ * @modifier underbar
+ *  [en]Displays a horizontal line underneath a select input.[/en]
+ *  [ja][/ja]
  * @description
  *   [en]
  *     Select component. If you want to place a select on a page, use `<ons-select>`.
  *
- *     Will automatically display as a Material Design select on Android.
+ *     The component will automatically display as a Material Design select on Android.
  *   [/en]
- *   [ja]ボタン用コンポーネント。ツールバーにボタンを設置する場合は、ons-toolbar-buttonもしくはons-back-buttonコンポーネントを使用します。[/ja]
+ *   [ja]ボタン用コンポーネント。ツールバーにボタンを設置する場合は、コンポーネントを使用します。[/ja]
  * @codepen hLayx
- * @tutorial vanilla/Reference/button
- * @guide Button [en]Guide for `<ons-button>`[/en][ja]<ons-button>の使い方[/ja]
+ * @tutorial vanilla/Reference/select
+ * @guide Select [en]Guide for `<ons-select>`[/en][ja]<ons-select>の使い方[/ja]
  * @guide using-modifier [en]More details about the `modifier` attribute[/en][ja]modifier属性の使い方[/ja]
  * @guide cross-platform-styling [en]Information about cross platform styling[/en][ja]Information about cross platform styling[/ja]
  * @example
- * <ons-button modifier="large--cta">
- *   Tap Me
- * </ons-button>
+ * <ons-select>
+ *   <option value="1">1</option>
+ *   <option value="2">2nd</option>
+ *   <option value="3">3rd option</option>
+ * </ons-select>
  */
 
 export default class SelectElement extends BaseElement {
 
   /**
-   * @attribute modifier
-   * @type {String}
+   * @attribute multiple
    * @description
-   *  [en]The appearance of the select.[/en]
-   *  [ja]ボタンの表現を指定します。[/ja]
-   */
-
-  /**
-   * @attribute ripple
-   * @description
-   *  [en]If this attribute is defined, the button will have a ripple effect.[/en]
+   *  [en]If this attribute is defined, multiple options can be selected at once.[/en]
    *  [ja][/ja]
    */
 
   /**
    * @attribute disabled
    * @description
-   *   [en]Specify if button should be disabled.[/en]
+   *   [en]Specify if select input should be disabled.[/en]
    *   [ja]ボタンを無効化する場合は指定します。[/ja]
    */
 
@@ -110,7 +83,7 @@ export default class SelectElement extends BaseElement {
   }
 
   static get observedAttributes() {
-    return ['modifier', 'class'];
+    return ['modifier', 'class', 'disabled'];
   }
 
   attributeChangedCallback(name, last, current) {
@@ -122,7 +95,19 @@ export default class SelectElement extends BaseElement {
         break;
       case 'modifier':
         ModifierUtil.onModifierChanged(last, current, this, scheme);
+        break;
+      case 'disabled':
+        if (this.hasAttribute('disabled')) {
+          this._select.setAttribute('disabled', this.getAttribute('disabled'));
+        }
+        else {
+          this._select.removeAttribute('disabled');
+        }
     }
+  }
+
+  get _select() {
+    return this.querySelector('select');
   }
 
   /**
